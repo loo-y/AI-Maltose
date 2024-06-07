db2b7fa2caa1edf842279795db770663a118ba6d
loo-y <loo-y@outlook.com>
2024-06-07 05:26:04 +0000

    clerk sign: invatitation only

--- app/(pages)/signin/[[...singin]]/page.tsx
+++ app/(pages)/signin/[[...singin]]/page.tsx
@@ -3,7 +3,13 @@ import { SignIn } from '@clerk/nextjs'
 export default function Page() {
     return (
         <div className="signmodule flex w-fit mx-auto my-10">
-            <SignIn />
+            <SignIn
+                appearance={{
+                    elements: {
+                        footerAction: { display: 'none' },
+                    },
+                }}
+            />
         </div>
     )
 }
--- app/(pages)/signup/[[...signup]]/page.tsx
+++ app/(pages)/signup/[[...signup]]/page.tsx
@@ -1,6 +1,14 @@
 import { SignUp } from '@clerk/nextjs'
+import { useSearchParams, redirect } from 'next/navigation'
 
 export default function Page() {
+    const searchParams = useSearchParams()
+    const clerkTicket = searchParams.get('__clerk_ticket')
+    if (!clerkTicket) {
+        redirect('/signin')
+        return
+    }
+
     return (
         <div className="signmodule flex w-fit mx-auto my-10">
             <SignUp />
