da90a32db431b1ade01ee5ca6bdac52c46e0a298
loo-y <loo-y@outlook.com>
2024-06-14 18:03:37 +0800

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 import { AI_BOT_TYPE } from '@/app/shared/interface'
 import { getAIBots } from '@/app/(routes)/graphql/dal/Supabase/queries'
+import _ from 'lodash'
 
 export default async function Home() {
     const { userId } = auth()
