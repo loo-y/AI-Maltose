697f809fcc953d47cbc2cbd6aac4ee3f5b4cdfab
loo-y <loo-y@outlook.com>
2024-06-17 10:21:04 +0000

    debugger

--- app/(pages)/after_signup/page.tsx
+++ app/(pages)/after_signup/page.tsx
@@ -8,16 +8,18 @@ export default async function Page({
     searchParams: { [key: string]: string | string[] | undefined }
 }) {
     const { userId } = auth()
-    console.log(`page userId`, userId)
+    console.log(`page after_signup userId`, userId)
     if (userId) {
         const user = await currentUser()
         const { username, emailAddresses } = user || {}
+        console.log(`page after_signup username`, username, emailAddresses)
         const userInfo = await createUser({
             username: username || '',
             userid: userId,
             email: emailAddresses?.[0]?.emailAddress || '',
             balance: 100,
         })
+        console.log(`page after_signup userInfo`, userInfo)
     }
 
     redirect('/')
