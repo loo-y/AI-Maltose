4cc339b29fe5bfdbd689336dc2cab2ca646dcffe
loo-y <loo-y@outlook.com>
2024-06-30 22:02:20 +0800

    feat: use imageid to swap

--- app/(routes)/api/faceswap/route.ts
+++ app/(routes)/api/faceswap/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server'
 const env = (typeof process != 'undefined' && process?.env) || ({} as NodeJS.ProcessEnv)
 import Replicate from 'replicate'
+import { imageUrlPrefix } from '@/app/shared/constants'
+
 const { REPLICATE_API_TOKEN } = env || {}
 
 const replicate = new Replicate({
@@ -9,6 +11,8 @@ const replicate = new Replicate({
 
 export const runtime = 'edge'
 
+const appDomain = 'https://sheepaw.com'
+
 export async function GET(request: NextRequest) {
     if (!REPLICATE_API_TOKEN) {
         return NextResponse.json({ message: 'REPLICATE_API_TOKEN not found' }, { status: 500 })
@@ -16,15 +20,23 @@ export async function GET(request: NextRequest) {
 
     const inputImageUrl = request.nextUrl.searchParams.get('inputImageUrl')
     const targetImageUrl = request.nextUrl.searchParams.get('targetImageUrl')
+    const inputID = request.nextUrl.searchParams.get('inputID')
+    const targetID = request.nextUrl.searchParams.get('targetID')
+
+    const swap_image = inputID ? `${appDomain}${imageUrlPrefix}/${inputID}` : inputImageUrl
+    const target_image = targetID ? `${appDomain}${imageUrlPrefix}/${targetID}` : targetImageUrl
+
+    const testTargetImage = `https://www.sheepaw.com/api/imageShow/Yi85ZmFpMmwyZTUvMzc3NGVkNTFkZDhlYTVhZjQyYjRkNmZhNDY4NmIzODNhYmVkMmIwMDYyMy5hajJwNmc=`
+    if (!swap_image || !target_image) {
+        return NextResponse.json({ message: 'inputImageUrl or targetImageUrl not found' }, { status: 500 })
+    }
     const output = await replicate.run(
         'omniedgeio/face-swap:c2d783366e8d32e6e82c40682fab6b4c23b9c6eff2692c0cf7585fc16c238cfe',
         {
             input: {
                 disable_safety_checker: true,
-                swap_image: inputImageUrl,
-                target_image:
-                    targetImageUrl ||
-                    'https://www.sheepaw.com/api/imageShow/Yi85ZmFpMmwyZTUvMzc3NGVkNTFkZDhlYTVhZjQyYjRkNmZhNDY4NmIzODNhYmVkMmIwMDYyMy5hajJwNmc=',
+                swap_image,
+                target_image: target_image,
             },
         }
     )
