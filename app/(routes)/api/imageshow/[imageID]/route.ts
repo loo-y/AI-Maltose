873ea948fd1b23ba00583be14b5db326f77a78ec
loo-y <loo-y@outlook.com>
2024-07-03 23:48:31 +0800

    feat: add cache-control when image show

--- app/(routes)/api/imageshow/[imageID]/route.ts
+++ app/(routes)/api/imageshow/[imageID]/route.ts
@@ -31,6 +31,7 @@ export async function GET(request: NextRequest, { params }: { params: { imageID:
             headers: {
                 'Content-Type': response.headers.get('Content-Type') || '',
                 'Content-Length': response.headers.get('Content-Length') || '',
+                'Cache-Control': 's-maxage=3600, stale-while-revalidate',
             },
         })
     } catch (error) {
